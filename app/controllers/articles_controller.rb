61b1fb57fa1fda38378c971a7100d1c49a0ee16e
Luis Enrique <luis.enri1392@gmail.com>
2017-02-20 17:36:08 -0600

    add edit, update , show listing ,create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,44 @@
 class ArticlesController < ApplicationController
+  def index
+  	@articles = Article.all
+  end
+
   def new
-  	@article = Article.new
-  	
+  	@article = Article.new	
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
   end
 
   def create
-    render plain: params[:articles].inspect
+    #render plain: params[:articles].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+ 	  flash[:notice] = "Article was successfully created"
+ 	  redirect_to article_path(@article)
+    else
+      render 'new'	
+    end
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+  	  flash[:notice] = "Article was successfully updated"
+  	  redirect_to article_path(@article)
+  	else
+  	  render 'edit'	
+  	end
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  	
   end
 
   private 
     def article_params
-      params.require(:articles).permit(:title, :description)
+      params.require(:article).permit(:title, :description)
     end
 end
