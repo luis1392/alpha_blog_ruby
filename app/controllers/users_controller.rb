a7ad1ff40ad06b4e23e9f4438b3518b6ca0f1810
Luis Enrique <luis.enri1392@gmail.com>
2017-02-25 19:35:42 -0600

    add edit func to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end	
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  	 flash[:success] = "Your accound was update successfully #{@user.username}"
+  	 redirect_to articles_path
+  	else
+
+  	end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)	
